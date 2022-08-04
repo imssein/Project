46993fb1f988230a24b84897ed38737be2fd8d31
sein <hyuntpdls@gmail.com>
2022-08-04 14:21:14 +0900

    feat: 로그인,마이페이지 redux 연동

--- frontend/components/user/UserProfile.jsx
+++ frontend/components/user/UserProfile.jsx
@@ -1,25 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import UserService from "../../services/user.service";
 import Link from "next/link";
 import { BsPencilSquare, BsBookmark } from "react-icons/bs";
+import { useDispatch } from 'react-redux';
+import { logoutAction } from "../../reducers/user";
+import Router from "next/router";
 
 function UserProfile(props) {
-  const [content, setContent] = useState([]);
+  const dispatch = useDispatch();
+  const onLogOut = useCallback(() => {
+    dispatch(logoutAction());
+    Router.push('/')
+  }, []);
+  // const [content, setContent] = useState([]);
 
-  useEffect(() => {
-    UserService.getMemberDetail().then(
-      (response) => {
-        setContent(response.data);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }, [setContent]);
+  // useEffect(() => {
+  //   UserService.getMemberDetail().then(
+  //     (response) => {
+  //       setContent(response.data);
+  //     },
+  //     (error) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }, [setContent]);
 
   return (
     <div className="text-center py-5 mx-5">
-      <div className="flex pb-6 border-b-2 mb-4">
+      {/* <div className="flex pb-6 border-b-2 mb-4">
         <div className="mr-10">
           <div className="rounded-full py-4 px-5 bg-gray-200 inline-block">
             현
@@ -40,8 +48,9 @@ function UserProfile(props) {
       <div className="flex border-b-2 py-4">
         <p className="my-auto"><BsBookmark size="30" /></p>
         <p className="ml-4 my-auto font-semibold ">피드</p>
-      </div>
-      
+      </div> */}
+                      <button onClick={onLogOut}>로그아웃</button>
+
 
     </div>
   );
