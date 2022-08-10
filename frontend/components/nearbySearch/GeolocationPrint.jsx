8e216a61761161bdd90ebfad74794f845aae0500
sein <hyuntpdls@gmail.com>
2022-08-10 18:36:02 +0900

    fix: 내주변 식당 조회 기능 수정

--- frontend/components/nearbySearch/GeolocationPrint.jsx
+++ frontend/components/nearbySearch/GeolocationPrint.jsx
@@ -1,34 +1,43 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BsFillGeoAltFill } from "react-icons/bs";
+import useGeolocation from "react-hook-geolocation";
+import GeolocationParams from "./GeolocationParams";
 
-function GeolocationPrint({latitude, longitude}) { 
-    const [address, setAddress] = useState("");
-    useEffect(() => {
-        if (longitude && latitude) {
-          axios
-            .get(
-              `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`,
-              {
-                headers: {
-                  Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_REST_API_KEY}`,
-                },
-              }
-            )
-            .then((result) => {
-              setAddress(result.data.documents[0].address_name);
-            });
-        } else {
-        }
-      });
-    return (
-        <div className="w-full"> 
-        <div className="flex my-6 justify-end">
-            <BsFillGeoAltFill size="20" className="mr-2"/>
-            <p className="font-medium">내 위치 : {address}</p>
-        </div>  
-        </div>
-    );
+function GeolocationPrint() {
+  const geolocation = useGeolocation();
+
+  const [address, setAddress] = useState("");
+  const [district, setDistrict] = useState("");
+
+  useEffect(() => {
+    if (!geolocation.error) {
+      axios
+        .get(
+          `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${geolocation.longitude}&y=${geolocation.latitude}`,
+          {
+            headers: {
+              Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_REST_API_KEY}`,
+            },
+          }
+        )
+        .then((result) => {
+          setAddress(result.data.documents[0].address_name);
+          setDistrict(result.data.documents[0].region_2depth_name);
+        });
+    } else {
+    }
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex my-6 justify-end">
+        <BsFillGeoAltFill size="20" className="mr-2" />
+        <p className="font-medium">내 위치 : {address}</p>
+      </div>
+      <GeolocationParams district={district} />
+    </div>
+  );
 }
 
 export default GeolocationPrint;
---
+++ frontend/components/nearbySearch/RestaurantsData.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import StoreService from '../../services/store.service';
+import DistrictMap from './DistrictMap';
+import RestaurantsList from './RestaurantsList';
+
+function RestaurantsData({params}) {
+    const [content, setContent] = useState([]);
+    console.log(params);
+
+   useEffect(() => {
+    if(params){
+    StoreService.getStoreDistrict({params}).then(
+        (response) => {
+            setContent(response.data)
+        }, 
+        (error) => {
+            console.log(error);
+        }
+    );
+    }
+    else { console.log("대기")}
+   }, [params, setContent]);
+    console.log({content})
+    return (
+        <div>
+            <DistrictMap content={content} />
+        </div>
+    );
+}
+
+export default RestaurantsData;
