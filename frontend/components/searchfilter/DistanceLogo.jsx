bca591fe2143598fef90390184201a853ccd8dc7
sein <hyuntpdls@gmail.com>
2022-08-15 00:35:09 +0900

    feat: 식당 위치와 사용자 위치 거리 계산

--- frontend/components/searchfilter/DistanceLogo.jsx
+++ frontend/components/searchfilter/DistanceLogo.jsx
@@ -1,57 +1,49 @@
 import React from "react";
-import { HiOutlineLocationMarker } from "react-icons/hi";
-const datas = [
-  {
-    id: 1,
-    title: "100m",
-  },
-  {
-    id: 2,
-    title: "200m",
-  },
-  {
-    id: 3,
-    title: "300m",
-  },
-  {
-    id: 4,
-    title: "400m",
-  },
-  {
-    id: 5,
-    title: "500m",
-  },
-  {
-    id: 6,
-    title: "1km",
-  },
-  {
-    id: 7,
-    title: "2km",
-  },
-  {
-    id: 8,
-    title: "3km",
-  },
-  {
-    id: 9,
-    title: "전체",
-  },
-];
-function DistanceLogo(props) {
-  return (
-    // <div className="my-6 grid grid-cols-5 gap-2 md:grid-cols-9 md:gap-2">
-    //   {datas.map((data) => (
-    //     <div key={data.id}> 
-    //       <button className="bg-stone-200 py-2 px-3 text-center w-16">{data.title}</button>
-    //     </div>
-    //   ))}
-    // </div>
-    <div className="flex rounded-full bg-slate-200 py-2 px-3">
-        <HiOutlineLocationMarker className="my-auto mr-1" />
-    <p>거리</p>
-  </div>
 
+function DistanceLogo({ dist_1, dist_2, dist_3, dist_4, dist_5, dist_6 }) {
+
+  function handleClick1(){
+    console.log("1", dist_1)
+  }
+  function handleClick2(){
+    console.log("2", dist_2)
+  }
+  function handleClick3(){
+    console.log("3", dist_3)
+  }
+  function handleClick4(){
+    console.log("4", dist_4)
+  }
+  function handleClick5(){
+    console.log("5", dist_5)
+  }
+  function handleClick6(){
+    console.log("6", dist_6)
+  }
+
+  return (
+    <div>
+      <div className="mt-6 flex">
+        <button className="bg-slate-200 py-3 px-9 w-full text-center hover:bg-slate-400 focus:bg-slate-400" onClick={handleClick1}>
+          300m
+        </button>
+        <button className="bg-slate-200 py-3 px-9 w-full text-center hover:bg-slate-400 focus:bg-slate-400" onClick={handleClick2}>
+          500m
+        </button>
+        <button className="bg-slate-200 py-3 px-9 w-full text-center hover:bg-slate-400 focus:bg-slate-400" onClick={handleClick3}>
+          1km
+        </button>
+        <button className="bg-slate-200 py-3 px-9 w-full text-center hover:bg-slate-400 focus:bg-slate-400" onClick={handleClick4}> 
+          3km
+        </button>
+        <button className="bg-slate-200 py-3 px-9 w-full text-center hover:bg-slate-400 focus:bg-slate-400" onClick={handleClick5}>
+          5km
+        </button>
+        <button className="bg-slate-200 py-3 px-9 w-full text-center hover:bg-slate-400 focus:bg-slate-400" onClick={handleClick6}>
+          전체
+        </button>
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/components/searchfilter/LatLng2Wtm.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import useGeolocation from "react-hook-geolocation";
+import DistanceCalculator from "./DistanceCalculator";
+
+function LatLng2Wtm() {
+  const geolocation = useGeolocation();
+  const [x, setX] = useState("");
+  const [y, setY] = useState("");
+
+  useEffect(() => {
+    if (!geolocation.error) {
+      axios
+        .get(
+            `https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${geolocation.longitude}&y=${geolocation.latitude}&input_coord=WGS84&output_coord=WTM`,
+          {
+            headers: {
+              Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_REST_API_KEY}`,
+            },
+          }
+        )
+        .then((result) => {
+          setX(result.data.documents[0].x);
+          setY(result.data.documents[0].y);
+        });
+    } else {
+    }
+  }, [geolocation.error, geolocation.latitude, geolocation.longitude, setX, setY]);
+
+  return (
+    <div>
+      {/* <p>사용자 위경도 - wtm</p>
+      <p>{x}</p>
+      <p>{y}</p> */}
+      <DistanceCalculator longitude={x} latitude={y} />
+    </div>
+  );
+}
+
+export default LatLng2Wtm;
