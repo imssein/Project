f3c91a5bcf4c0b85ca1fb20c5e5d3d9b45b03fe5
sein <hyuntpdls@gmail.com>
2022-08-17 16:14:57 +0900

    fix: 검색 컴포넌트 변경

--- frontend/components/intro/index.jsx
+++ frontend/components/intro/index.jsx
@@ -6,21 +6,18 @@ import Menu from "./Menu";
 import Title from "./Title";
 import VegeInfoBtn from "./VeganGuide";
 import Information from "../information";
-import Search from "../search";
+import SearchMain from "../search/SearchMain";
 
 function Intro(props) {
   return (
     <div className="px-4 py-8">
       {/* 검색창 */}
-      {/* <SearchForm /> */}
-      <Search />
+      <SearchMain />
       {/* 사이트설명 */}
-
       <Title
         title="맛있는 채식 한끼"
         description="지역을 클릭하면 해당 구의 채식 식당을 확인 할 수 있습니다."
       />
-
       {/* 지도 */}
       <SeoulMap />
       {/* 채식타입에 따른 식당 조회 */}
