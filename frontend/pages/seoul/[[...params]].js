e7aaf6d47999ac742d61ac5eec754359944a5b60
sein <hyuntpdls@gmail.com>
2022-09-04 20:48:25 +0900

    fix: 자치구별 식당 조회 수정

--- frontend/pages/seoul/[[...params]].js
+++ frontend/pages/seoul/[[...params]].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 import Title from "../../components/intro/Title";
 import SeoulData from "../../components/seoulMap/SeoulData";
+import DistrictContainers from "../../components/district/containers/DistrictContainers";
 
 function Map() {
   const router = useRouter();
@@ -20,7 +21,8 @@ function Map() {
           <Title title="맛있는 채식 한끼" description="지역을 클릭하면 해당 구의 채식 식당을 확인 할 수 있습니다." />
         </div>
         {/* 지역구명 */}
-        <SeoulData params={params} />
+        {/* <SeoulData params={params} /> */}
+        <DistrictContainers params={params} />
       </div>
     </div>
   );
