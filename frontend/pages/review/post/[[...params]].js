cc82edd55521c3af23f84cb1d514c3452af0799e
sein <hyuntpdls@gmail.com>
2022-09-22 15:00:33 +0900

    feat: 리뷰 관련 기능 구현 및 수정

---
+++ frontend/pages/review/post/[[...params]].js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useRouter } from "next/router";
+import MainLayout from "../../../components/common/MainLayout";
+import Head from "next/head";
+import ReviewForm from "../../../components/reviewComponent/Reviewform";
+
+function Post(props) {
+  const router = useRouter();
+  const { params = [] } = router.query;
+  console.log(params);
+
+  return (
+    <MainLayout>
+      <div>
+        <Head>
+          <title>VeganPleasure | 리뷰 작성</title>
+        </Head>
+        <div className="py-4 mx-9">
+          <ReviewForm params={params} />
+        </div>
+      </div>
+    </MainLayout>
+  );
+}
+
+export default Post;
