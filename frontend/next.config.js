c1db9df991e36d377c7a840d70892d81c4f691d3
sein <hyuntpdls@gmail.com>
2022-11-05 15:11:47 +0900

    refactor: next.config.js 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
+const nextConfig = {
+  reactStrictMode: false,      // true -> false로 바꿔준다. 
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  }
+}
 module.exports = nextConfig
 
 module.exports = {
   images: {
-      domains: ['images.unsplash.com']
+      domains: [
+        'images.unsplash.com', 
+        
+      ]
   }
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
